9440421d4d69a4b449eda58c747360a99b94e93e
ajinkya101 <bhabalajinkya@gmail.com>
2024-02-18 14:44:28 +0530

    skip check for key

--- main.tf
+++ main.tf
@@ -21,7 +21,6 @@ resource "azurerm_storage_account" "storeaccount" {
     }
   }
 
-  shared_access_key_enabled     = false
   public_network_access_enabled = false
 
   identity {
@@ -43,4 +42,6 @@ resource "azurerm_storage_account" "storeaccount" {
   #checkov:skip=CKV2_AZURE_38: "Ensure soft-delete is enabled on Azure storage account"
   #checkov:skip=CKV2_AZURE_1: "Ensure storage for critical data are encrypted with Customer Managed Key"
   #checkov:skip=CKV_AZURE_190: "Ensure that Storage blobs restrict public access"
+  #checkov:skip=CKV2_AZURE_40: "Ensure storage account is not configured with Shared Key authorization"
+
 }
